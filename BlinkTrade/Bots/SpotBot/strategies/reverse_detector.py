5504b6d2ad9caca25b13211286b2bf2bf5aafc94
yikhong2309 <116650944+yikhong2309@users.noreply.github.com>
2024-01-12 21:10:54 +0800

    Update file path

--- BlinkTrade/Bots/SpotBot/strategies/reverse_detector.py
+++ BlinkTrade/Bots/SpotBot/strategies/reverse_detector.py
@@ -1,119 +1,217 @@
-"""
-File: reverse_detector.py
-Author: Henry Miao
-Date Created: 2023-10-03
-Last Modified: --
-Description: Use machine learning to detect reversion of price.
-"""
 import pandas as pd
 import pickle
 import ta
+import os
+import sys
+import xgboost as xgb
 
+sys.path.append('c:\\Users\\Admin\\Desktop')
 
 class Reverse_Detector(object):
-    def __init__(self, model_save_path, model_name):
-        self.ml_model = self.load_ml_model(model_save_path, model_name)
 
-    def load_ml_model(self, model_save_path, model_name):
-        with open(model_save_path + model_name, 'rb') as f:
+    def __init__(self, model_save_path):
+
+        self.model_save_path = model_save_path
+        self.xgb_model = self.load_xgb_models(model_save_path)
+    
+    def read_model(self, model_path):
+
+        with open(model_path, 'rb') as f:
             model = pickle.load(f)
-        return model
 
-    def get_machine_learning_pridictions(self, factor_df):
+        return model
+    
+    def load_xgb_models(self, model_save_path = 'C:\\Users\\Admin\\Desktop\\Crypto_Bot\\models\\'):
+
+        model_name = 'best_xgboost_rich_model_60_full_data.pkl'
+        model_path = os.path.join(model_save_path, model_name)
+
+        xgb_model = self.read_model(model_path)
+
+        return xgb_model
+    
+    def get_machine_learning_predictions(self, factor_df):
         '''
-        :param factor_df: 传入的数据，包含了所有的因子
+        :param factor_df: transferred data, including all alphas
+        :param direction: transferred direction, up/down
         '''
         data_df = factor_df.copy()
-        y_predict = self.ml_model.predict_proba(data_df.iloc[-1:, :])
-        y_rise_prob = y_predict[0][2]
-        y_fall_prob = y_predict[0][0]
-        return y_rise_prob, y_fall_prob
+        y_predict = self.xgb_model.predict_proba(data_df.iloc[-1:, :])
 
-
+        return y_predict[0][1]
+    
 class Features_Calculator(object):
+
     def __init__(self):
-        self.funcName_list = ['hl2', 'ohlc4', 'tema', 'rsi', 'wcp', 'cmo', 'bias', 'zlma',
-                              'hlc3', 'zscore', 'dm', 'pgo', 'midpoint', 'decreasing',
-                              'decay', 'ui', 'aroon', 'cdl_z', 'vortex', 'amat']  # pandas_ta中的函数名
 
-        self.factorName_list = ['HL2', 'OHLC4', 'TEMA_10', 'RSI_14', 'WCP', 'CMO_14', 'BIAS_SMA_26',
-                                'ZL_EMA_10', 'HLC3', 'ZS_30', 'DMN_14', 'PGO_14', 'MIDPOINT_2', 'DEC_1',
-                                'LDECAY_5', 'UI_14', 'AROOND_14', 'close_Z_30_1', 'VTXM_14', 'AMATe_SR_8_21_2']  # 因子名
+        # Feature X(s) based on ta
+        self.X_cols = [
+            'rtn_1', 'OCHL_range', 'trend_adx', 'trend_cci', 'macd', 'momentum_rsi',
+            'theta_close', 'theta_volume', 'theta_quote_volume', 'theta_rtn_1',
+            'theta_OCHL_range', 'theta_volume_obv', 'theta_volume_vwap',
+            'diff_theta_volume', 'diff_theta_volume_obv', 'diff_rtn_1',
+            'diff_OCHL_range', 'diff_theta_close', 'diff_trend_adx',
+            'diff_trend_cci', 'diff_momentum_rsi', 'diff_macd',
+        ]
 
-        # 单个币种的全部特征
-        self.X_standalone_cols = self.factorName_list  # 20个
+        self.co_diff_target_cols = ['momentum_rsi', 'theta_volume_obv', 'macd', 'trend_cci', 'trend_adx']
 
-        # 交互特征  10个
-        self.co_diff_target_cols = (['DMN_14', 'ZS_30', 'CMO_14', 'RSI_14', 'UI_14', 'PGO_14',
-                                     'close_Z_30_1', 'VTXM_14', 'BIAS_SMA_26', 'AROOND_14'])  # 10个
+        # Create 5 new cols for both btc and eth-based diff targets ('momentum_rsi', 'theta_volume_obv', 'macd', 'trend_cci', 'trend_adx')
         self.co_diff_target_cols_btc = ['co_diff_' + x + "_btc" for x in self.co_diff_target_cols]
         self.co_diff_target_cols_eth = ['co_diff_' + x + "_eth" for x in self.co_diff_target_cols]
 
-        # btc eth自身的特征  20*2 = 40个
-        self.btc_X_cols = [x + "_btc" for x in self.X_standalone_cols]
-        self.eth_X_cols = [x + "_eth" for x in self.X_standalone_cols]
+        self.btc_X_cols = [x + "_btc" for x in self.X_cols]
+        self.eth_X_cols = [x + "_eth" for x in self.X_cols]
 
-        # 所有X_cols
-        self.all_X_cols = self.X_standalone_cols + self.co_diff_target_cols_btc + self.co_diff_target_cols_eth + self.btc_X_cols + self.eth_X_cols
-        self.useful_X_Cols = self.X_standalone_cols + self.co_diff_target_cols_btc + self.co_diff_target_cols_eth  ## 目前来看更有效的特征子集
+        # Total X columns for ML model
+        self.all_X_cols = self.X_cols + self.btc_X_cols + self.eth_X_cols + self.co_diff_target_cols_btc + self.co_diff_target_cols_eth
         return
 
     def save_market_coin_data(self, data_df, coin_name):
-        '''
-        保存单个币种的数据
-        '''
-        factor_df = self.add_all_standalone_features(data_df)[self.X_standalone_cols]
+
+        factor_df = self.get_all_features(data_df)[self.X_cols]
         factor_df.columns = factor_df.columns + '_' + coin_name
+
         if coin_name == 'btc':
             self.btc_factor_data = factor_df
+
         elif coin_name == 'eth':
             self.eth_factor_data = factor_df
+
         return
+    
+    def add_ta_features(self, factor_df, raw_df):
+        data_df = raw_df[['open', 'close', 'high','low', 'volume']]
 
-    def get_all_features_add_market_coin(self, data_df):
-        '''
-            计算全部技术指标
-            然后将该货币的指标，以及btc，eth指标进行合并
+        factor_df['trend_adx'] = ta.trend.adx(data_df['high'], data_df['low'], data_df['close'])
+        factor_df['trend_cci'] = ta.trend.cci(data_df['high'], data_df['low'], data_df['close'])
+        factor_df['macd'] = ta.trend.MACD(data_df['close']).macd()
+        factor_df['momentum_rsi'] = ta.momentum.rsi(data_df['close'])
+        factor_df['volume_obv'] = ta.volume.on_balance_volume(data_df['close'],data_df['volume'])
+        factor_df['volume_vwap'] = ta.volume.volume_weighted_average_price(data_df['high'], data_df['low'],
+                                                                           data_df['close'],data_df['volume'])
 
-            return factor_df
-        '''
-        # 1. 计算全部技术指标
-        factor_df = self.add_all_standalone_features(data_df)[self.X_standalone_cols]
-        # 2. 与保存的btc eth数据进行合并
-        factor_df = self.combine_features(factor_df, self.btc_factor_data, self.eth_factor_data)
-        factor_df = self.add_co_diff_features_market_coin(factor_df)  # 加入互相关因子
-        return factor_df[self.all_X_cols]
+        return factor_df
+    
+    def _calculate_theta(self, target_column, price_df, theta_interval):
+        '''Calculate each theta'''
 
-    def add_all_standalone_features(self, data_df):
+        theta_df = pd.DataFrame()
+
+        # Calculate theta based on EMA
+        # target_column = closing price
+        theta_df['mean_20_' + target_column] = price_df[[target_column]].ewm(span=theta_interval, adjust=False).mean()
+        theta_df['std_20_' + target_column] = price_df[[target_column]].ewm(span=theta_interval, adjust=False).std()
+        theta_df[target_column] = price_df[target_column]
+
+        target_column_theta = (theta_df[target_column] - theta_df['mean_20_' + target_column]) / theta_df['std_20_' + target_column]
+
+        return target_column_theta
+
+    def calculate_theta_all(self, factor_df, price_df, theta_interval_list=[], target_column_list=["close"]):
         '''
-        计算全部技术指标：
+        Calculating theta value
+        factor_df: dataframe that stored theta 
+        price_df：dataframe that stored price data
+        '''
+        for i in range(len(target_column_list)):
+
+            if len(theta_interval_list) != 0:
+                theta_interval = theta_interval_list[i]
+            else:
+                theta_interval = 20
+            target_column = target_column_list[i]
+
+            # calculate theta = (p - ma) / sigma
+            factor_df['theta_' + target_column] = self._calculate_theta(target_column, price_df, theta_interval)
+
+        return factor_df
+
+    def add_rtn_feature(self, factor_df, raw_df):
+        '''
+        calculate 15 mins relative return based on prior row - "close" column
+        '''
+        factor_df['rtn_1'] = (raw_df['close'] - raw_df['close'].shift(1)) / raw_df['close'].shift(1)
+
+        return factor_df
+
+    def add_OCHL_feature(self, factor_df, raw_df):
+        '''
+        calculate OCHL_range to determine volatility range
+        '''
+        factor_df['OCHL_range'] = (raw_df['close'] - raw_df['open']) / (raw_df['high'] - raw_df['low'])
+        factor_df['OCHL_range'].fillna(1e-4, inplace=True)
+
+        return factor_df
+
+    def add_all_diff_features(self, factor_df: pd.DataFrame, target_columns):
+        '''
+        Create a diff col for each features to de-noise
+
+            diff_theta_volume
+            diff_theta_volume_obv
+            diff_rtn_1
+            diff_OCHL
+            diff_theta_close
+            diff_adx
+            diff_cci
+            diff_momentum_rsi
+            diff_macd
+        '''
+        for feature_name in target_columns:
+            factor_df['diff_' + feature_name] = factor_df[feature_name] - factor_df[feature_name].shift(1)
+
+        return factor_df
+
+    def get_all_features(self, data_df):
+        '''
+            计算全部独立的技术指标：
             1. 原始指标（部分不会直接用到）
             2. 原始指标的 theta 指标 （归一化）
             3. 原始指标的 diff 指标
+
+            return factor_df
         '''
-        raw_cols = ['open', 'high', 'low', 'close', 'quote_volume', 'open_time']
+
+        raw_cols = ['open', 'high', 'low', 'close', 'volume', 'quote_volume', ]
         data_df = data_df[raw_cols].copy()
+
+        factor_df = pd.DataFrame()
+
         # 1 原始指标
-        ### ta 的技术指标
-        data_df = self.add_ta_features(data_df)
-        return data_df
+        ### OCHL_range 和 rtn (data_df = raw_df)
+        factor_df = self.add_rtn_feature(factor_df, data_df)
+        factor_df = self.add_OCHL_feature(factor_df, data_df)
 
-    def add_ta_features(self, raw_df):
-        '''
-        计算 pandas ta 原始技术指标
-        '''
-        raw_df.rename({'quote_volume': 'volume'}, axis=1, inplace=True)
-        for indicator_func_name in self.funcName_list:  # 遍历全部所需函数，计算对应特征
-            fun = getattr(raw_df.ta, indicator_func_name)
-            temp_data = fun(append=True)
-        return raw_df
+        ### ta 的技术指标 (data_df = raw_df)
+        factor_df = self.add_ta_features(factor_df, data_df)
 
-    def cal_Z_score(self, factor_df, column_lists, span):
-        '''Z-score'''
-        for column_name in column_lists:
-            mean_20 = factor_df[column_name].ewm(span, adjust=False).mean()
-            std_20 = factor_df[column_name].ewm(96, adjust=False).std()  # todo 超参数搜索
-            factor_df[f'{column_name}_Z'] = (factor_df[column_name].values - mean_20) / std_20
+        ### 1.1 Theta指标
+        target_columns_list = ['close', 'volume', 'quote_volume']  # 需要计算theta的对应列
+        factor_df = self.calculate_theta_all(factor_df, data_df, target_column_list=target_columns_list)
+        factor_df = self.calculate_theta_all(factor_df, factor_df, [20, 20], target_column_list=['rtn_1', 'OCHL_range'])
+        factor_df = self.calculate_theta_all(factor_df, factor_df, [20, 20], target_column_list=['volume_obv', 'volume_vwap'])
+
+        factor_df.drop(['volume_obv', 'volume_vwap'], axis=1, inplace=True)
+
+        ### 1.2 diff 指标
+        diff_target_columns = ['theta_volume',
+                               'theta_volume_obv',
+                               'rtn_1',
+                               'OCHL_range',
+                               'theta_close',
+                               'trend_adx',
+                               'trend_cci',
+                               'momentum_rsi',
+                               'macd']
+        
+        factor_df = self.add_all_diff_features(factor_df, diff_target_columns)
+
+        assert data_df.shape[0] == factor_df.shape[0]
+
+        factor_df['close'] = data_df['close']
+        factor_df.dropna(inplace=True)
+
         return factor_df
 
     def combine_features(self, factor_df, btc_data, eth_data):
@@ -124,24 +222,37 @@ class Features_Calculator(object):
         '''
         加入互相关关系的因子，也就是coin和btc eth的差值
         '''
-        co_diff_target_cols = self.co_diff_target_cols
-        for feature_name in co_diff_target_cols:
+        for feature_name in self.co_diff_target_cols:
             factor_df['co_diff_' + feature_name + "_btc"] = factor_df[feature_name] - factor_df[feature_name + "_btc"]
             factor_df['co_diff_' + feature_name + "_eth"] = factor_df[feature_name] - factor_df[feature_name + "_eth"]
         return factor_df
 
+    def get_all_features_add_market_coin(self, data_df):
+        '''
+            计算全部技术指标
+            然后将该货币的指标，以及btc，eth指标进行合并
 
+            return factor_df
+        '''
+        factor_df = self.get_all_features(data_df)[self.X_cols]
+        factor_df = self.combine_features(factor_df, self.btc_factor_data, self.eth_factor_data)  # 合并
+        factor_df = self.add_co_diff_features_market_coin(factor_df)  # 加入互相关因子
+        return factor_df[self.all_X_cols]
+
+## Example
 if __name__ == '__main__':
-    from BlinkTrade.BlinkTrade.Bots.SpotBot.utils.data_utils import get_market_prices
+
+    from Crypto_Bot.utils.data_utils import get_market_prices
 
     btc_price_df = get_market_prices('BTCUSDT', '15m')
     eth_price_df = get_market_prices('ETHUSDT', '15m')
 
+
     print(btc_price_df.head())
     f_c = Features_Calculator()
 
-    f_c.save_market_coin_data(btc_price_df, coin_name='btc')
-    f_c.save_market_coin_data(eth_price_df, coin_name='eth')
+    f_c.save_market_coin_data(btc_price_df, coin_name='btc' )
+    f_c.save_market_coin_data(eth_price_df, coin_name='eth' )
 
     price_df = get_market_prices('AXSUSDT', '15m')
 
@@ -149,8 +260,11 @@ if __name__ == '__main__':
 
     print(factor_df.head())
 
+
     # 测试预测部分
-    model_save_path = '../models/'
+    # Older pandas version (1.5.0)
+    # Older xgboost version (1.2.0)
+    model_save_path = 'C:/Users/Admin/Desktop/Crypto_Bot/models/'
     r_d = Reverse_Detector(model_save_path)
-    y_predict = r_d.get_machine_learning_pridictions(factor_df)
+    y_predict = r_d.get_machine_learning_predictions(factor_df)
     print(y_predict)
