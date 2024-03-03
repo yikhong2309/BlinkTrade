6ff4ed6d25c6df48f7809b43f2d17ee40b850086
yikhong2309 <116650944+yikhong2309@users.noreply.github.com>
2024-03-03 11:55:08 +0800

    Add files via upload
    API input in batches

--- BlinkTrade/Bots/SpotBot/utils/information.py
+++ BlinkTrade/Bots/SpotBot/utils/information.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from binance.spot import Spot as SpotClient
-
 import sys
 sys.path.append(r'C:\Users\Admin\Desktop\\')
 
@@ -8,7 +7,27 @@ from Crypto_Bot.utils.data_utils import get_decimal_precision
 from Crypto_Bot.utils.privateconfig import *
 
 
-## Parent Class
+class Info_Controller():
+    '''信息控制器'''
+    def __init__(self, config_dict, client, use_strategy=True):
+        self.use_strategy = use_strategy
+
+        self.account_info = Account_Info(config_dict)
+        if self.use_strategy:
+            self.strategy_info = Strategy_Info(config_dict)
+        self._init_info_all(client)
+
+    def _init_info_all(self, client):
+        self.account_info.init_info(client)
+        if self.use_strategy:
+            self.strategy_info.init_info(client)
+
+    def update_info_all(self, client):
+        self.account_info.update_info(client)
+        if self.use_strategy:
+            self.strategy_info.update_info(client)
+
+
 class Info_Interface():
     def __init__(self):
         pass
@@ -19,16 +38,14 @@ class Info_Interface():
     def update_info(self, client):
         pass
 
-## Subclass 1
+
 class Account_Info(Info_Interface):
     '''
-    Account Info, Current position info & Historical position info
+    账户的信息
+    持仓的信息,或者历史持仓信息
     '''
     def __init__(self, config_dict):
-
-        # The super().__init__() line is calling the __init__ method of the parent class (Info_Interface) within the __init__ method of the Account_Info class.
         super().__init__()
-
         self.position_df = None
         self.USDT_value = None
         self.account_value = None
@@ -40,46 +57,53 @@ class Account_Info(Info_Interface):
             2. init "is_hold"
             3. init "bid_price" for the holds
         '''
-        # Get account info
-        account_infomation = client.account()  
+        account_infomation = client.account()  # Get account info
 
-        # Account balances info
+        # account balances info
         position_df = pd.DataFrame(account_infomation['balances'])
         self.USDT_value = float(position_df[position_df["asset"] == "USDT"]['free'].values[0])
 
-        # Get actual price of "XXXUSDT"
+        # Get acg price of "XXXUSDT"
         # Get all real market symbols
         exchange_dict = client.exchange_info()
         temp = exchange_dict['symbols']
         market_symbols = set([symbol_dict['symbol'] for symbol_dict in temp])
 
-        # We only need currencies pair with USDT
+        # 只需要对应有USDT交易对的即可
         # 此处实际上得到了全部的持仓信息，包括了不在候选交易对中的
         fake_symbols = [x + 'USDT' for x in position_df['asset'].values]
         position_df['symbol'] = fake_symbols
-
         is_symbol_exist = [True if x in market_symbols else False for x in fake_symbols]
-
-        # all symbol with USDT and Exist
-        exist_symbols = list(position_df[is_symbol_exist].symbol.values) 
+        exist_symbols = list(position_df[is_symbol_exist].symbol.values)  # all symbol with USDT and Exsit
         position_df = position_df[is_symbol_exist]
 
-        price_df = pd.DataFrame(client.ticker_price(symbols=exist_symbols))
+        ##########################################
+        batch_size = 10
+        symbol_batches = [exist_symbols[i:i + batch_size] for i in range(0, len(exist_symbols), batch_size)]
+
+        # Iterate over batches and make API calls
+        price_dfs = []
+        for symbols_batch in symbol_batches:
+            price_df_batch = pd.DataFrame(client.ticker_price(symbols=symbols_batch))
+            price_dfs.append(price_df_batch)
+
+        # Concatenate all price dataframes
+        price_df = pd.concat(price_dfs)
+
         position_df = pd.merge(position_df, price_df, on='symbol', how='left')
         position_df.set_index('symbol', inplace=True, drop=True)
+        ##########################################
 
-        # Price data Formatting 
+        # 格式转换
         convert_dict = {'free': float,
                         'locked': float,
                         'price': float,
                         }
         position_df = position_df.astype(convert_dict)
-
-        # Calculate if it is already in position
+        # 计算是否已经持仓
         position_df["balance"] = position_df["free"] * position_df["price"]
         position_df["is_hold"] = position_df["balance"] > 5
-
-        # 用当前价格，初始化已经持仓的成本价。"bid_price" -> cost price
+        # 用当前价格，初始化已经持仓的成本价。"bid_price"
         position_df["bid_price"] = 0.001
         condition = position_df["is_hold"]
         position_df.loc[condition, "bid_price"] = position_df.loc[condition, "price"]
@@ -88,71 +112,70 @@ class Account_Info(Info_Interface):
         self.account_value = self.position_df["balance"].sum()
 
         return
-    
+
+
     def update_info(self, client):
         '''
         update free, locked value
         '''
         position_df = self.position_df
-        
-        # Get Latest account info
-        account_infomation = client.account()  
-        new_position_df = pd.DataFrame(account_infomation['balances'])
 
+        account_infomation = client.account()  # Get new account info
+        new_position_df = pd.DataFrame(account_infomation['balances'])
         # update USDT_value
         self.USDT_value = float(new_position_df[new_position_df["asset"] == "USDT"]['free'].values[0])
 
-        # Drop and renew each latest positions
+
         position_df.drop(columns=["free", "locked", "price", "balance", "is_hold"], inplace=True)
         position_df.reset_index(inplace=True)
-
         position_df = pd.merge(position_df, new_position_df, on="asset", how='left')
         position_df.set_index('symbol', inplace=True, drop=True)
 
-        # Update price data
-        price_df = pd.DataFrame(client.ticker_price(symbols=list(position_df.index.values)))
+        # 更新价格
+        ##########################################
+        all_symbols = list(position_df.index.values)
+        batch_size = 10
+        symbol_batches = [all_symbols[i:i + batch_size] for i in range(0, len(all_symbols), batch_size)]
+        price_lists = []
+        for symbols_batch in symbol_batches:
+            temp_price_list = client.ticker_price(symbols=symbols_batch)
+            price_lists += temp_price_list
+        price_df = pd.DataFrame(price_lists)
+        ###########################################
+
         position_df = pd.merge(position_df, price_df, left_index=True, right_on='symbol', how='left')
         position_df.set_index('symbol', inplace=True, drop=True)
 
-        # Formatting
+        # 格式转换
         convert_dict = {'free': float,
                         'locked': float,
                         'price': float,
                         }
-        
         position_df = position_df.astype(convert_dict)
-
-        # Calculate if it is already in position
+        # 计算是否已经持仓
         position_df["balance"] = position_df["free"] * position_df["price"]
         position_df["is_hold"] = position_df["balance"] > 5
 
-        #  Update account_value
         self.position_df = position_df
-        self.account_value = self.position_df["balance"].sum()     
+        self.account_value = self.position_df["balance"].sum()     #  更新account_value
         return
-    
-    def get_symbols_held_sets(self):
 
+    def get_symbols_held_sets(self):
         held_set = set(self.position_df[self.position_df["is_hold"]].index.values)
         un_held_set = set(self.position_df[~self.position_df["is_hold"]].index.values)
         return held_set, un_held_set
-    
-## Subclass 2
+
+
 class Strategy_Info(Info_Interface):
-    '''
-    Strategy info
-    '''
+    '''策略的信息'''
     def __init__(self, config_dict):
-
-        # The super().__init__() line is calling the __init__ method of the parent class (Info_Interface) within the __init__ method of the Strategy_Info class.
         super().__init__()
-
         self.quote_currency = config_dict['quote_currency']
-        self.candidate_symbols = config_dict['candidate_symbols']       
-        self.base_currencies = [x[:-4] for x in config_dict['candidate_symbols']] # Remove 'USDT' from candidate symbols
-        
+        self.candidate_symbols = config_dict['candidate_symbols']
+        self.base_currencies = [x[:-4] for x in config_dict['candidate_symbols']]
         return
-    
+
+
     def init_info(self, client):
         '''
         self.trade_info_df:
@@ -170,7 +193,8 @@ class Strategy_Info(Info_Interface):
 
         self.price_dict = dict()  # price_dict
         return
-    
+
+
     def check_symbols(self, client):
         '''
         检查候选的交易对是否存在于市场交易对象中
@@ -182,74 +206,36 @@ class Strategy_Info(Info_Interface):
         market_symbols = set([symbol_dict['symbol'] for symbol_dict in exchange_dict['symbols']])
 
         symbol_in_market = True
-
         for symbol in self.candidate_symbols:
-
             if symbol not in market_symbols:
                 print(f"Warning: {symbol} is not in market_symbols")
                 self.candidate_symbols.remove(symbol)
                 symbol_in_market = False
                 continue
-
         if symbol_in_market:
             print("All candidate symbols are in market_symbols")
         return
-    
-    
+
+
     def update_price_dict(self, price_dict):
         self.price_dict = price_dict
         return
 
+
     def get_trade_decimal_precision(self, client):
-
         self.theta_info_df["stepDecimal"] = 2
-
         for temp_symbol in self.candidate_symbols:
-
             print("temp_symbol:", temp_symbol)
-
             temp_info_dict = client.exchange_info(symbol=temp_symbol)
             temp_stepSize = temp_info_dict['symbols'][0]['filters'][1]['stepSize']
             temp_decimal_precision = get_decimal_precision(temp_stepSize)
-
             self.theta_info_df.loc[temp_symbol,"stepDecimal"] = temp_decimal_precision
 
         pass
 
-class Info_Controller():
-    '''
-    info controller
-    '''
-    def __init__(self, config_dict, client, use_strategy=True):
-
-        self.use_strategy = use_strategy
-
-        self.account_info = Account_Info(config_dict)
-
-        if self.use_strategy:
-            self.strategy_info = Strategy_Info(config_dict)
-
-        self._init_info_all(client)
-
-    def _init_info_all(self, client):
-
-        self.account_info.init_info(client)
-
-        if self.use_strategy:
-            self.strategy_info.init_info(client)
-
-    def update_info_all(self, client):
-
-        self.account_info.update_info(client)
-
-        if self.use_strategy:
-            self.strategy_info.update_info(client)
 
 
-## Example
-            
 if __name__ == "__main__":
-
     util_config = dict(
         candidate_symbols=['AUTOUSDT', 'ETHBULLUSDT', 'BNBBULLUSDT', 'BNBBEARUSDT', 
         'SUSHIDOWNUSDT', 'TRBUSDT', 'AAVEUSDT', 'BTGUSDT', 'EOSBEARUSDT', 'BCHSVUSDT', 
@@ -262,12 +248,10 @@ if __name__ == "__main__":
         'COCOSUSDT', 'OGUSDT'],
         quote_currency='usdt',
     )
-
     strategy_config = dict(
         market_t=100,
         fraction=0.3,  # 买入比例
     )
-    
     config_dict = dict()
     config_dict.update(util_config)
     config_dict.update(strategy_config)
@@ -280,9 +264,11 @@ if __name__ == "__main__":
                 "https://api4.binance.com"]
 
     # API key/secret are required for user data endpoints
-    client = SpotClient(api_key=g_api_key,
-                        api_secret=g_secret_key,
-                        base_url=API_urls[3])
+    client = SpotClient(
+        api_key=g_api_key,
+        api_secret=g_secret_key,
+        base_url=API_urls[3]
+    )
 
     info_c = Info_Controller(config_dict, client)
     # print(info_c.account_info.position_df)
